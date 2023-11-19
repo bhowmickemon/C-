4c748cb6f6aa1ead7f769d04d88dd3b4ff33f7db
Emon Bhowmick <emonbhowmickbe@gmail.com>
2023-11-19 22:54:07 +0600

    Add files via upload

---
+++ admin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace project
+{
+    public partial class admin : Form
+    {
+        public admin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-CMR1CLF;Initial Catalog=Project_data;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From Login where User_name='" + textBox1.Text + "'and Password='" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+
+                this.Hide();
+                admin2 ss = new admin2();
+                ss.Show();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect User name or Password");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 ff=new Form1();
+            ff.Show();
+
+        }
+    }
+}
